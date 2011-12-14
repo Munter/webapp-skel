fc405ed45551d877b48439a5eb091062595a4e43
Peter Müller <munter@fumle.dk>
2011-12-14 16:17:30 +0100

    Updated bootstrap.sh to install node 0.6

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ sudo apt-get install git libssl-dev libcairo2-dev libgif-dev libjpeg8-dev optipn
 cd ~
 git clone git://github.com/joyent/node
 cd node
-git checkout v0.4
+git checkout v0.6
 make distclean # Only necessary if you've compiled from the same checkout before
 mkdir ~/.local # If it doesn't already exist
 ./configure --prefix=~/.local
@@ -18,9 +18,3 @@ make install
 export PATH=~/.local/bin:${PATH}
 # Optionally
 # echo "export PATH=~/.local/bin:$PATH" >> ~/.bashrc
-
-# Install NPM
-curl http://npmjs.org/install.sh | sh
-
-# Install assetgraph-builder
-npm install -g assetgraph-builder
